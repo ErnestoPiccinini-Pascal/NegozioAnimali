e7e770634effc1405322475ec21d96380d97bf88
Ernesto <ernesto.piccinini@studenti.iispascal.it>
2026-03-21 10:14:34 +0100

    sistematutto

---
+++ NegozioAnimali/src/negozioanimali/Animale.java
@@ -0,0 +1,34 @@
+package negozioanimali;
+
+public abstract class Animale {
+
+    private String nome;
+    private int eta;
+
+    // Costruttore
+    public Animale(String nome, int eta) {
+        this.nome = nome;
+        this.eta = eta;
+    }
+
+    // Getter
+    public String getNome() {
+        return nome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    // Metodo astratto: verso dell'animale
+    public abstract String verso();
+
+    // Metodo astratto: descrizione dell'animale
+    public abstract String descrizione();
+
+    // Metodo concreto toString()
+    @Override
+    public String toString() {
+        return descrizione();
+    }
+}
